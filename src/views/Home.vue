ca50f9cf606df1f72ad628be30951675737e8413
TrollinPanda <a.bowdler@live.com>
2020-01-23 16:18:55 -0600

    Vue Pokedex
    Pokedex written in vue.js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="select-pokemon-page">
+    <h1>VueDex</h1>
+    <div class="select-pokemon-content">
+      <pokemon-list
+        :pokemon-list="statePokemonDataList"
+        :favorites="stateFavoritePokemonList"
+        @deleteFavorite="deleteFavorite"
+        @addFavorite="addFavorite"
+      />
+      <summary-favorites
+        :pokemon-list="statePokemonDataList"
+        :favorites="stateFavoritePokemonList"
+        @addFavorite="addFavorite"
+        @eraseFavoritePokemonList="eraseFavoritePokemonList"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import PokemonList from '@/components/PokemonList'
+import SummaryFavorites from '@/components/SummaryFavorites'
+import { mapState, mapActions } from 'vuex'
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+    components: {
+        PokemonList,
+        SummaryFavorites,
+    },
+    computed: {
+        ...mapState(['statePokemonDataList', 'stateFavoritePokemonList']),
+    },
+    async created() {
+        const pokemonData = await this.getPokemonData()
+        this.setPokemonData(pokemonData)
+    },
+    methods: {
+        async getPokemonData() {
+            const data = await fetch(
+                'https://pokeapi.co/api/v2/pokemon?limit=808'
+            )
+            const json = await data.json()
+            return json.results
+        },
+        ...mapActions(['setPokemonData', 'addFavorite', 'deleteFavorite', 'eraseFavoritePokemonList']),
+    }
+}
 </script>
+
+<style scoped>
+.select-pokemon-page {
+    text-align: center;
+}
+
+.select-pokemon-content {
+    text-align: left;
+    display: flex;
+    justify-content: space-evenly;
+}
+</style>
+
